e3934c97af6402f6e404af326a62fef221d94696
Brian Godinez <55292738+BrianGodinez@users.noreply.github.com>
2022-12-25 14:07:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const button = document.getElementById("christmas-button");
 button.addEventListener("click", function() {
-  alert("Merry Christmas! Wishing you a happy holiday season.");
+  alert("Merry Christmas! I love you bella.");
 });
